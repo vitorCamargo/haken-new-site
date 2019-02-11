f93c2228422da457d5827d3563d2ffb087e8b850
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-02-11 15:33:33 -0200

    Menu Total Responsivo

--- js/hamburguer-menu.js
+++ js/hamburguer-menu.js
@@ -1,15 +1,18 @@
 function toggleMenu(x) {
 	x.classList.toggle("change");
 	
-	if(document.body.classList.contains("changeBody"))
+	if(document.body.classList.contains("changeBody")) {
 		document.getElementById("body").classList.toggle("changeBody");
-	else
+		document.getElementById("html").classList.toggle("changeHTML");
+	} else {
 		setTimeout(function() {
 			document.getElementById("body").classList.toggle("changeBody");
+			document.getElementById("html").classList.toggle("changeHTML");
 		}, 100);
+	}
 }
 
-function clickBody(x) {
-	if(x.classList.contains("changeBody"))
+function clickBody() {
+	if(document.body.classList.contains("changeBody"))
 		toggleMenu(document.getElementById("hamburguer-menu"));
 }
